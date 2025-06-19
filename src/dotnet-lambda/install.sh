9c348eea76fde52b577c1c3977761e6335f1623d
scott.baldwin <scott.baldwin@purple.telstra.com>
2025-06-19 00:55:14 +0000

    Fixed issues and added more diagnostics
    - Changed elif to else
    - Added check for dotnet cli
    - Added more echo statements for better diagnostics
    - Print out the Amazon.Lambda.Tools version after installation.

--- src/dotnet-lambda/install.sh
+++ src/dotnet-lambda/install.sh
@@ -1,15 +1,25 @@
 #1/bin/bash
 set -e
 
+echo "Checking for dotnet cli"
+if ! command -v dotnet &> /dev/null; then
+    echo "dotnet CLI could not be found. Please install .NET SDK first."
+    exit 1
+fi
+
 echo "Installing .NET Lambda Tools and templates..."
 echo "the specified version is ${VERSION}"
 
 echo "Installing .NET Lambda Tools..."
 if [ "${VERSION}" != "latest" ]; then
+    echo "installing latest version of Amazon.Lambda.Tools"
     dotnet tool install -g Amazon.Lambda.Tools
-elif
+else
+    echo "installing version ${VERSION} of Amazon.Lambda.Tools"
     dotnet tool install -g Amazon.Lambda.Tools --version "${VERSION}"
 fi
+
+dotnet tool list -g Amazon.Lambda.Tools
 
 dotnet new -i "Amazon.Lambda.Templates::*"
 echo "Dotnet templates installed, available templates are"
