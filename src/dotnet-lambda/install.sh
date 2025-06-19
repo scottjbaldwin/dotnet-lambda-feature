6fd9b89487ac37985098107bfafa11e4c148e13f
scott.baldwin <scott.baldwin@purple.telstra.com>
2025-06-19 15:32:44 +1000

    Remove check for dotnet cli

--- src/dotnet-lambda/install.sh
+++ src/dotnet-lambda/install.sh
@@ -1,26 +1,20 @@
 #1/bin/bash
 set -e
 
-echo "Checking for dotnet cli"
-if ! command -v /bin/dotnet &> /dev/null; then
-    echo "dotnet CLI could not be found. Please install .NET SDK first."
-    exit 1
-fi
-
 echo "Installing .NET Lambda Tools and templates..."
 echo "the specified version is ${VERSION}"
 
 echo "Installing .NET Lambda Tools..."
 if [ "${VERSION}" != "latest" ]; then
     echo "installing latest version of Amazon.Lambda.Tools"
-    /bin/dotnet tool install -g Amazon.Lambda.Tools
+    dotnet tool install -g Amazon.Lambda.Tools
 else
     echo "installing version ${VERSION} of Amazon.Lambda.Tools"
-    /bin/dotnet tool install -g Amazon.Lambda.Tools --version "${VERSION}"
+    dotnet tool install -g Amazon.Lambda.Tools --version "${VERSION}"
 fi
 
-/bin/dotnet tool list -g Amazon.Lambda.Tools
+dotnet tool list -g Amazon.Lambda.Tools
 
-/bin/dotnet new -i "Amazon.Lambda.Templates::*"
+dotnet new -i "Amazon.Lambda.Templates::*"
 echo "Dotnet templates installed, available templates are"
-/bin/dotnet new list --author AWS
+dotnet new list --author AWS
