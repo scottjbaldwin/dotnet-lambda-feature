5627124820354c64419d2e90b989735d07ede107
scott.baldwin <scott.baldwin@purple.telstra.com>
2025-06-19 22:07:04 +1000

    Added check for templates

--- test/latest.sh
+++ test/latest.sh
@@ -3,5 +3,6 @@ set -e
 source dev-container-features-test-lib
 
 check "dotnet lambda" dotnet tool list -g | grep "amazon.lambda.tools"
+check "dotnet lambda templates" dotnet new list --author AWS | grep "serverless.AspNetCoreWebAPI"
 
 reportResults
