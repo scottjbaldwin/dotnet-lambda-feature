2060ae261f81e94bc980a4bdaebfa372ac53a8b4
scott.baldwin <scott.baldwin@purple.telstra.com>
2025-06-19 16:35:11 +1000

    Change to install dotnet for the remote user
    - Use sudo -H -u ... to install dotnet lambda for the $_REMOTE_USER
    - Update the version

--- src/dotnet-lambda/install.sh
+++ src/dotnet-lambda/install.sh
@@ -4,17 +4,17 @@ set -e
 echo "Installing .NET Lambda Tools and templates..."
 echo "the specified version is ${VERSION}"
 
-echo "Installing .NET Lambda Tools..."
+echo "Installing .NET Lambda Tools for user ${_REMOTE_USER}..."
 if [ "${VERSION}" = "latest" ]; then
     echo "installing latest version of Amazon.Lambda.Tools"
-    dotnet tool install -g Amazon.Lambda.Tools
+    sudo -H -u $_REMOTE_USER bash -c 'dotnet tool install -g Amazon.Lambda.Tools'
 else
     echo "installing version ${VERSION} of Amazon.Lambda.Tools"
-    dotnet tool install -g Amazon.Lambda.Tools --version "${VERSION}"
+    sudo -H -u $_REMOTE_USER bash -c 'dotnet tool install -g Amazon.Lambda.Tools --version "${VERSION}"'
 fi
 
-dotnet tool list -g Amazon.Lambda.Tools
+sudo -H -u $_REMOTE_USER bash -c 'dotnet tool list -g Amazon.Lambda.Tools'
 
-dotnet new -i "Amazon.Lambda.Templates::*"
+sudo -H -u $_REMOTE_USER bash -c 'dotnet new -i "Amazon.Lambda.Templates::*"'
 echo "Dotnet templates installed, available templates are"
-dotnet new list --author AWS
+sudo -H -u $_REMOTE_USER bash -c 'dotnet new list --author AWS'
