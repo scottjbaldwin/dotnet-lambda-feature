07ad747b7cc5c347605f11e3d12d3c795fe2abbb
scott.baldwin <scott.baldwin@purple.telstra.com>
2025-06-19 13:39:29 +1000

    Update the install to use full path to dotnet
    - Use /bin/dotnet instead of assuming it's in the path
    - Update to version 1.0.1

--- src/dotnet-lambda/install.sh
+++ src/dotnet-lambda/install.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Checking for dotnet cli"
-if ! command -v dotnet &> /dev/null; then
+if ! command -v /bin/dotnet &> /dev/null; then
     echo "dotnet CLI could not be found. Please install .NET SDK first."
     exit 1
 fi
@@ -13,14 +13,14 @@ echo "the specified version is ${VERSION}"
 echo "Installing .NET Lambda Tools..."
 if [ "${VERSION}" != "latest" ]; then
     echo "installing latest version of Amazon.Lambda.Tools"
-    dotnet tool install -g Amazon.Lambda.Tools
+    /bin/dotnet tool install -g Amazon.Lambda.Tools
 else
     echo "installing version ${VERSION} of Amazon.Lambda.Tools"
-    dotnet tool install -g Amazon.Lambda.Tools --version "${VERSION}"
+    /bin/dotnet tool install -g Amazon.Lambda.Tools --version "${VERSION}"
 fi
 
-dotnet tool list -g Amazon.Lambda.Tools
+/bin/dotnet tool list -g Amazon.Lambda.Tools
 
-dotnet new -i "Amazon.Lambda.Templates::*"
+/bin/dotnet new -i "Amazon.Lambda.Templates::*"
 echo "Dotnet templates installed, available templates are"
-dotnet new list --author AWS
+/bin/dotnet new list --author AWS
